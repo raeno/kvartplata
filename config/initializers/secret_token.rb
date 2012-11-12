23da12cc07513e7e5f60eaf68eeb248f8a99a9ea
raeno <just.raeno@gmail.com>
2012-11-12 22:11:57 +0400

    Default rails app for kvartplata project migration from c# to rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kvartplata::Application.config.secret_token = '1d96e27d76006dae4f25e08f4dc254589cff890c966454163869637131c4fac07720012be57c02f0e5cf5eaf1cc4d48216ab5e18a3a2712adeec6eb17cf9b20f'
