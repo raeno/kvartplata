a20fbd1200ae472c9461024e4652bb93a8277df4
raeno <just.raeno@gmail.com>
2013-03-06 15:04:17 +0400

    Previous metric value validation moved to separate previous_record_value  validator.

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,17 +1,20 @@
+require "previous_record_value_validator"
+
 class Metric < ActiveRecord::Base
+  include ActiveModel::Validations
+
   attr_accessible :cold_counter_kitchen, :cold_counter_bathroom, :hot_counter_kitchen, :hot_counter_bathroom,
                   :energy_counter, :month
   validates_presence_of :cold_counter_kitchen, :cold_counter_bathroom,
                         :hot_counter_kitchen, :hot_counter_bathroom, :energy_counter
 
-  #validates_numericality_of :cold_counter_kitchen, :greater_than => Proc.new { |r| r.previous_metric.cold_counter_kitchen }, :allow_blank => true
-  validates_each :cold_counter_kitchen, :cold_counter_bathroom,
-                 :hot_counter_kitchen, :hot_counter_bathroom, :energy_counter do |record,attr,value|
-    record.errors.add(attr, "must be greater than #{record.previous_metric.send(attr)}") if value <= record.previous_metric.send(attr)
-  end
+  validates_that_greater_than_a_previous_value :cold_counter_kitchen, :cold_counter_bathroom,
+                 :hot_counter_kitchen, :hot_counter_bathroom, :energy_counter
 
 
-  def previous_metric
+
+
+  def previous_record
     @previous ||= Metric.where('month < ?',month).last
   end
 
