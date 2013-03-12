7585d442a6c0c766fd1c23a06479068f2dd59ca8
raeno <just.raeno@gmail.com>
2013-03-12 09:34:57 +0400

    FIxed validation for previous metric value.

--- app/middleware/previous_record.rb
+++ app/middleware/previous_record.rb
@@ -4,7 +4,7 @@ module ClientSideValidations::Middleware
       month = DateTime.now
       prev_value =  ::Metric.where('month < ?',month).last.send(request.params[:attr])
       a = request.params[:metric].to_f
-      if a >= prev_value
+      if a > prev_value
         self.status = 200
       else
         self.status = 404
