5a90d0231233fc934cb49cd90e329f51d33eb31c
raeno <just.raeno@gmail.com>
2013-03-14 13:24:34 +0400

    New feature: when date is near payment date and there are no data for this month in db - user redirected to new_metric form. Otherwise report for current or last month rendered.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,13 @@ class WelcomeController < ApplicationController
 
   def index
 
-
-
+    if Metric.time_to_pay?
+      redirect_to new_metric_path
+    else
+      @report = Report.last
+      respond_to do |format|
+        format.html # show.html.erb
+      end
+    end
   end
 end
--- test/functional/welcome_controller_test.rb
+++ test/functional/welcome_controller_test.rb
@@ -8,17 +8,19 @@ class WelcomeControllerTest < ActionController::TestCase
   end
 
   test 'should redirect to new metric if no metric yet and date near payment date' do
-    date = 5.years.since.change(:days => Metric::PAYMENT_DAY)
+    date = 5.years.since.change(:day => Metric::PAYMENT_DAY)
     Timecop.freeze(date)
     get :index
     assert_redirected_to new_metric_path
   end
 
   test 'should show report if this month metric already in database and date near payment date' do
-    date = 5.years.since.change(:days => Metric::PAYMENT_DAY)
+    date = 5.years.since.change(:day => Metric::PAYMENT_DAY)
     Timecop.freeze(date)
 
-    create(:metric)
+    create(:metric, month: 2.month.ago)
+    create(:metric, month: 1.month.ago)
+    create(:metric, month: DateTime.now)
 
     get :index
     assert_response :success
