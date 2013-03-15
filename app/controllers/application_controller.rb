6ffa9c51c9d0ac6374fde288b668e0c886de3d5b
raeno <just.raeno@gmail.com>
2013-03-15 11:38:27 +0400

    More stylization for report table. Added correct utilities output.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   helper_method :last_reports, :current_tariff
+  before_filter :set_i18n_locale_from_params
 
   def last_reports
     Report.all(:order => 'date DESC', :limit => 3)
@@ -9,6 +10,22 @@ class ApplicationController < ActionController::Base
 
   def current_tariff
     Tariff.last
+  end
+
+  protected
+  def set_i18n_locale_from_params
+    if params[:locale]
+      if I18n.available_locales.include? params[:locale].to_sym
+        I18n.locale = params[:locale]
+      else
+        flash.now[:notice] = "#{params[:locale]} translation not available"
+        logger.error flash.now[:notice]
+      end
+    end
+  end
+
+  def default_url_options
+    {locale: I18n.locale }
   end
 
 end
