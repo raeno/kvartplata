841f628f0cd709fd61cddd4d793900e5b5b41b04
raeno <just.raeno@gmail.com>
2013-03-26 15:18:18 +0400

    Added tests for metrics create and update methods when they fall validation.

--- test/functional/metrics_controller_test.rb
+++ test/functional/metrics_controller_test.rb
@@ -11,6 +11,20 @@ class MetricsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:metrics)
   end
 
+  test "should correctly return page if locale supported" do
+    get :index, :locale => 'ru'
+    assert_response :success
+    assert_not_nil assigns(:metrics)
+  end
+
+  test "should show notice when locale is not supported" do
+    get :index, :locale => 'br'
+    assert_response :success
+    assert flash[:notice]
+    assert_equal flash[:notice], 'br translation not available'
+    assert_not_nil assigns(:metrics)
+  end
+
   test "should get new" do
     get :new
     assert_response :success
@@ -20,8 +34,14 @@ class MetricsControllerTest < ActionController::TestCase
     assert_difference('Metric.count') do
       post :create, metric: FactoryGirl.attributes_for(:metric)
     end
-
     assert_redirected_to Report.last
+  end
+
+  test "should render new metric form if validations failed for metric" do
+    assert_no_difference('Metric.count') do
+      post :create, metric: FactoryGirl.attributes_for(:metric, :hot_counter_kitchen => -2)
+    end
+    assert_template 'new'
   end
 
   test "should show metric" do
@@ -39,6 +59,13 @@ class MetricsControllerTest < ActionController::TestCase
     assert_redirected_to metric_path(assigns(:metric))
   end
 
+  test 'should redirect to metric edit if metric validations failed' do
+    put :update, id: @metric, metric: { :hot_counter_kitchen => -2}
+    assert_response :success
+    assert_template 'edit'
+  end
+
+
   test "should destroy metric" do
     assert_difference('Metric.count', -1) do
       delete :destroy, id: @metric
@@ -46,4 +73,6 @@ class MetricsControllerTest < ActionController::TestCase
 
     assert_redirected_to metrics_path
   end
+
+
 end
