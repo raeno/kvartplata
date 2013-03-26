99c4e89bb55dd61a564e734d9d7469197c009e07
raeno <just.raeno@gmail.com>
2013-03-26 15:19:09 +0400

    Double quotes changed to single quotes for test method names.

--- test/functional/metrics_controller_test.rb
+++ test/functional/metrics_controller_test.rb
@@ -5,19 +5,19 @@ class MetricsControllerTest < ActionController::TestCase
     @metric = create(:metric)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:metrics)
   end
 
-  test "should correctly return page if locale supported" do
+  test 'should correctly return page if locale supported' do
     get :index, :locale => 'ru'
     assert_response :success
     assert_not_nil assigns(:metrics)
   end
 
-  test "should show notice when locale is not supported" do
+  test 'should show notice when locale is not supported' do
     get :index, :locale => 'br'
     assert_response :success
     assert flash[:notice]
@@ -25,36 +25,36 @@ class MetricsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:metrics)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create metric" do
+  test 'should create metric' do
     assert_difference('Metric.count') do
       post :create, metric: FactoryGirl.attributes_for(:metric)
     end
     assert_redirected_to Report.last
   end
 
-  test "should render new metric form if validations failed for metric" do
+  test 'should render new metric form if validations failed for metric' do
     assert_no_difference('Metric.count') do
       post :create, metric: FactoryGirl.attributes_for(:metric, :hot_counter_kitchen => -2)
     end
     assert_template 'new'
   end
 
-  test "should show metric" do
+  test 'should show metric' do
     get :show, id: @metric
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get :edit, id: @metric
     assert_response :success
   end
 
-  test "should update metric" do
+  test 'should update metric' do
     put :update, id: @metric, metric: {  }
     assert_redirected_to metric_path(assigns(:metric))
   end
@@ -66,7 +66,7 @@ class MetricsControllerTest < ActionController::TestCase
   end
 
 
-  test "should destroy metric" do
+  test 'should destroy metric' do
     assert_difference('Metric.count', -1) do
       delete :destroy, id: @metric
     end
