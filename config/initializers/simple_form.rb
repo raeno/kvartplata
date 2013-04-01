41d3effb11870d80ad8336b37de232f5de1a9060
raeno <just.raeno@gmail.com>
2013-04-01 13:42:23 +0400

    Added login dropdown to navbar.

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -140,9 +140,13 @@ SimpleForm.setup do |config|
   # Cache SimpleForm inputs discovery
   # config.cache_discovery = !Rails.env.development?
 
-  config.wrappers :checkbox, :tag => 'div', :class => 'control-group', :error_class => 'error' do |b|
-    b.use :tag => 'div', :class => 'custom-checkbox' do |ba|
-      ba.use :label_input
+  config.wrappers :inline_checkbox, :tag => 'div', :class => 'control-group', :error_class => 'error' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.wrapper :tag => 'div', :class => 'controls' do |ba|
+      ba.use :label_input, :class => 'checkbox inline'
+      ba.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
+      ba.use :hint,  :wrap_with => { :tag => 'p', :class => 'help-block' }
     end
   end
 end
