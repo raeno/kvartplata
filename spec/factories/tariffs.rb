467266318d4c5413ceb01b666b92e91c042ea50b
raeno <just.raeno@gmail.com>
2013-05-20 11:59:52 +0400

    Factory for each model moved to a separate file.

---
+++ spec/factories/tariffs.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+
+  factory :tariff do
+    start_date 2.years.ago
+    end_date 1000.years.since
+    cold_water  12.74
+    hot_water  90.86
+    utilities  7.64
+    energy  3.23
+  end
+
+end
