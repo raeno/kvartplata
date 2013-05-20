37768d0b2e237c44836e5ae262c0178f7c0ce0bc
raeno <just.raeno@gmail.com>
2013-05-20 11:54:02 +0400

    Refactored SettingsController specs.

--- spec/controllers/settings_controller_spec.rb
+++ spec/controllers/settings_controller_spec.rb
@@ -2,13 +2,25 @@ require 'spec_helper'
 
 describe SettingsController do
 
-  before do
-    signed_in_as_a_valid_user
-    create :tariff
-  end
+  before { create :tariff }
 
-  it 'should get index' do
-    get :index
-    response.should be_success
+  describe 'GET index' do
+
+    context 'when not logged in' do
+      it 'redirects to sign in page' do
+        get :index
+        response.should redirect_to new_user_session_path
+      end
+    end
+
+    context 'when logged in' do
+
+      before { signed_in_as_a_valid_user }
+
+      it 'assigns current_user as user' do
+        get :index
+        response.should be_success
+      end
+    end
   end
 end
