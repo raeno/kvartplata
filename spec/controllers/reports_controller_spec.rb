4c38f193c081f55775e1e7efbbb6280323b43ad2
raeno <just.raeno@gmail.com>
2013-05-20 11:46:47 +0400

    Refactored ReportsController specs. Added specs to  'when not logged in' context.

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -3,37 +3,51 @@ require 'spec_helper'
 describe ReportsController do
 
   let(:valid_attributes) { attributes_for(:report, date: DateTime.new(2012,3,2).round_to_month) }
-
-  before do
-    @apartment_info = create(:apartment_info)
-    signed_in_as_a_valid_user
-  end
-
+  let(:report) { create :report }
 
   describe 'GET index' do
-    context 'assigns all reports as reports_by_year' do
-      before do
-        @report = Report.create! valid_attributes
-        get :index, {}
-        @reports =  assigns(:reports_by_year)
-      end
 
-      it { @reports.should be_kind_of Hash }
-      it { @reports.count.should == 1 }
-      it { @reports[2012].should be_kind_of Hash }
-      it { @reports[2012].count.should == 1 }
-      it { @reports[2012][3].should eq [@report] }
+    context 'when not logged in' do
+      it 'redirects to sign in page' do
+        get :index
+        response.should redirect_to new_user_session_path
+      end
+    end
+
+    context 'when logged in' do
+      before { signed_in_as_a_valid_user }
+
+      describe 'assigns all reports as reports_by_year' do
+        before do
+          @report = Report.create! valid_attributes
+          get :index, {}
+          @reports =  assigns(:reports_by_year)
+        end
+
+        it { @reports.should be_kind_of Hash }
+        it { @reports.count.should == 1 }
+        it { @reports[2012].should be_kind_of Hash }
+        it { @reports[2012].count.should == 1 }
+        it { @reports[2012][3].should eq [@report] }
+      end
     end
   end
 
   describe 'GET show' do
-    it 'assigns the requested report as @report' do
-      report = Report.create! valid_attributes
-      get :show, { :month => report.date.month, :year => report.date.year }
-      assigns(:report).should eq(report)
+    context 'when not logged in' do
+      it 'redirects to sign in page' do
+        get :show, :month => report.date.month, :year => report.date.year
+        response.should redirect_to new_user_session_path
+      end
+    end
+
+    context 'when logged in' do
+      before { signed_in_as_a_valid_user }
+
+      it 'assigns the requested report as @report' do
+        get :show, { :month => report.date.month, :year => report.date.year }
+        assigns(:report).should eq(report)
+      end
     end
   end
-
-
-
 end
